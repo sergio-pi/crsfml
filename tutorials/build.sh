d58dd9600616a98bf434a45714bcfbf0b0587230
Robot <>
2016-08-08 10:20:19 +0000

    Generate tutorials (077cd14)

---
+++ tutorials/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -o errexit
+
+PATH="node_modules/.bin${PATH:+:$PATH}"
+
+if ! command -v gitbook; then
+    npm install gitbook-cli
+fi
+
+gitbook install gitbook
+gitbook build .
